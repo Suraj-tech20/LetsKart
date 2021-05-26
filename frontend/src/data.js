eba6aec64f416aadd936da0632c585d77d04e77a
Suraj garg <garg69850@gmail.com>
2021-05-26 12:49:57 +0530

    Sharing with github

---
+++ frontend/src/data.js
@@ -0,0 +1,77 @@
+const data = {
+    products: [{
+
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+
+            _id: '2',
+            name: 'Adidas fit Shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 100,
+            brand: 'Adidas',
+            rating: 4.2,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+
+        {
+
+            _id: '3',
+            name: 'Lacoste free Shirt',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 220,
+            brand: 'Lacoste',
+            rating: 4.8,
+            numReviews: 17,
+            description: 'high quality product'
+        },
+        {
+
+            _id: '4',
+            name: 'Nike Slim Pant',
+            category: 'Pant',
+            image: '/images/p4.jpg',
+            price: 200,
+            brand: 'Nike',
+            rating: 4.0,
+            numReviews: 12,
+            description: 'high quality product'
+        },
+        {
+
+            _id: '5',
+            name: 'Puma slim Pants',
+            category: 'Pants',
+            image: '/images/p5.jpg',
+            price: 100,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 20,
+            description: 'high quality product'
+        },
+        {
+
+            _id: '6',
+            name: 'Adidas Fit Pant',
+            category: 'Pants',
+            image: '/images/p6.jpg',
+            price: 120,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 18,
+            description: 'high quality product'
+        }
+    ]
+}
+export default data;
