602fba447dc2532c96204dee51c1cd10704a70b9
Suraj garg <garg69850@gmail.com>
2021-05-29 19:50:05 +0530

    include backend and fatch data from it using proxy in package.json and intall axios in frontend include loading icon

---
+++ backend/data.js
@@ -0,0 +1,83 @@
+const data = {
+    products: [{
+
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+
+            _id: '2',
+            name: 'Adidas fit Shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 100,
+            countInStock: 50,
+            brand: 'Adidas',
+            rating: 4.2,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+
+        {
+
+            _id: '3',
+            name: 'Lacoste free Shirt',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 220,
+            countInStock: 15,
+            brand: 'Lacoste',
+            rating: 4.8,
+            numReviews: 17,
+            description: 'high quality product'
+        },
+        {
+
+            _id: '4',
+            name: 'Nike Slim Pant',
+            category: 'Pant',
+            image: '/images/p4.jpg',
+            price: 200,
+            countInStock: 30,
+            brand: 'Nike',
+            rating: 4.0,
+            numReviews: 12,
+            description: 'high quality product'
+        },
+        {
+
+            _id: '5',
+            name: 'Puma slim Pants',
+            category: 'Pants',
+            image: '/images/p5.jpg',
+            price: 100,
+            countInStock: 0,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 20,
+            description: 'high quality product'
+        },
+        {
+
+            _id: '6',
+            name: 'Adidas Fit Pant',
+            category: 'Pants',
+            image: '/images/p6.jpg',
+            price: 120,
+            countInStock: 25,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 18,
+            description: 'high quality product'
+        },
+    ]
+}
+export default data;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import data from '../data';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+// import data from '../data';
 import Product from '../components/Products';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 export default function HomeScreen() {
+    // Hooks to to show loading screeen
+    const [loading, setLoading] = useState(false);
+    //Hook for error in feching data from backend
+    const [error, setError] = useState(false);
+    // Hooks to fectch data from backend
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        const fectData = async () => {
+            try {
+                setLoading(true);
+                const { data } = await axios.get('/api/products');
+                setLoading(false);
+                setProducts(data);
+            } catch (err) {
+                setError(err.message);
+                setLoading(false);
+            }
+        };
+        fectData();
+    }, [])
     return (
-        <div className="row center">
-            {data.products.map((product) => {
-                return (
-                    <>
-                        <Product key={product._id} product={product}></Product>
-                    </>
-                )
-            })
+        <div>
+            {loading ? <LoadingBox></LoadingBox> : error ? <MessageBox varient="danger">{error}</MessageBox> :
+                (<div className="row center">
+                    {products.map((product) => {
+                        return (
+                            <>
+                                <Product key={product._id} product={product}></Product>
+                            </>
+                        )
+                    })
+                    }
+                </div>)
             }
         </div>
     )
